2d6781addf7ab81fe0382e96788eee1f173ff5b0
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-05 02:24:04 +0900

    1.Create Constant file and add some Key strings, 2.

---
+++ SlideshowApp/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  SlideshowApp
+//
+//  Created by 櫻井将太郎 on 2020/12/05.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+	static let BAR_BUTTON_IMAGE_play: String = "play.fill"
+
+	static let BAR_BUTTON_IMAGE_pause: String = "pause.fill"
+
+	static let SEGUE_ID: String = ""
+}
