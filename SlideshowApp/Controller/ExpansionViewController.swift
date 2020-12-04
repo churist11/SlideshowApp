b48153b5a54ceced45999778e8eff252abbfa49b
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-12-04 23:25:23 +0900

    Add 2 views and controllers

---
+++ SlideshowApp/Controller/ExpansionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExpansionViewController.swift
+//  SlideshowApp
+//
+//  Created by 櫻井将太郎 on 2020/12/04.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+// From SlideViewController, When user tapped image view.
+class ExpansionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+	}
+
+}// End
---
+++ SlideshowApp/Controller/SlideViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SlideViewController.swift
+//  SlideshowApp
+//
+//  Created by 櫻井将太郎 on 2020/12/04.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class SlideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}// End
